57d91ca9b955f518a8d5303842e45a20cb61a9c0
ederson <ederson.villegas@amihansolutions.com>
2024-01-11 17:33:04 +0800

    initial sign in sign up FE

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import Link from 'next/link'
+
+export default function Copyright(props: any) {
+  return (
+    <Typography variant='body2' color='text.secondary' align='center' {...props}>
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        QuickEd sales
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
